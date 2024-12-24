2d9a273d54965cfc3f5d5a5fcc5986032cd446a7
draell <joaoduos@gmail.com>
2024-12-23 23:23:29 -0300

    O inicio de um bispo

---
+++ src/br/ufrn/imd/model/Pieces/Bishop.java
@@ -0,0 +1,19 @@
+package br.ufrn.imd.model.Pieces;
+
+import br.ufrn.imd.model.Position2D;
+import br.ufrn.imd.model.Side;
+
+public class Bishop extends Piece {
+
+    public Bishop (Position2D new_position, Side new_side) {
+        this.current_position = new Position2D(new_position);
+        this.side             = new_side; // vamos ter problemas com referencia aqui?
+        this.value            = 3;
+
+    }
+
+    @Override
+    public boolean movable(Position2D candidate_position) {
+        return false;
+    }
+}
